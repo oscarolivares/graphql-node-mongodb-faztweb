2d2f1ccaf54ec405b65d8f04545c7ad780214428
Oscar Olivares <oscarolivares625@gmail.com>
2019-07-13 06:20:46 -0400

    Implementacion de un schema ejecutable (Separa la definicion del schema y de los resolvers en archivos separados)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import express from "express";
+import graphqlHTTP from "express-graphql";
+import schema from "./schema";
+
 
 const app = express();
+
+
+app.get('/', (req, res) => {
+  res.redirect('/graphql');
+})
+
+app.use('/graphql', graphqlHTTP({
+  graphiql: true,
+  schema: schema
+}))
+
 
 app.listen(3000, () => console.log('Server on port 3000'));
